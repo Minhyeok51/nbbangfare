02b4dc0313546d5dc82ca123789a43a02bec884d
minhyeok <minhyeok51@naver.com>
2022-12-22 17:31:46 +0900

    로그인 만들기 시작

--- nbbanfare/src/main/frontend/src/component/Login.js
+++ nbbanfare/src/main/frontend/src/component/Login.js
@@ -1,14 +1,63 @@
-import "../css/login.css"
+import { useState } from "react";
+import "../css/login.css";
+import { useNavigate } from "react-router-dom";
 
-function Login(){
-    return(
-        <div className="loginContainer">
-            <form>
-                <input type="text"></input>
-                <input type="password"></input>
-            </form>
+function Login() {
+  let [id, setId] = useState("");
+  let [pw, setPw] = useState("");
+  const realId = "asdc";
+  const realPw = "asd123!!";
+  let navigate = useNavigate();
+  const [button, setButton] = useState(true);
+  function changeButton() {
+    pw.length >= 5 ? setButton(false) : setButton(true);
+  }
+  const goToMain = () => {
+    navigate("/");
+  };
 
-        </div>
-    )
+  return (
+    <div className="loginContainer">
+      <form className="loginForm">
+        <fieldset className="loginFormInner">
+            <legend className="loginText">로그인</legend>
+        <input
+          placeholder="아이디"
+          id="id"
+          className="login"
+          onChange={(e) => {
+            setId(e.target.value);
+          }}
+          onKeyUp={changeButton}
+        />
+        <input
+          type="password"
+          placeholder="비밀번호"
+          id="password"
+          className="login"
+          onChange={(e) => {
+            setPw(e.target.value);
+          }}
+          onKeyUp={changeButton}
+        />
+        <button
+          type="button"
+          className="loginButton"
+          disabled={button}
+          onClick={(e) => {
+            if (realId == id) {
+              if (realPw == pw) {
+                e.stopPropagation();
+                goToMain();
+              }
+            } else {
+              alert("아이디 혹은 비밀번호가 일치하지 않습니다.");
+            }
+          }}
+        >로그인</button>
+        </fieldset>
+      </form>
+    </div>
+  );
 }
 export default Login;
